b2c27d5c118ab3402f0724a679ca135b031d9de7
Marvin <marvin@kickner.bayern>
2024-08-28 18:40:44 +0200

    Add documentation for rememberIt helper function

--- LoadingDialogs/src/main/java/bayern/kickner/loadingdialogs/ExtensionsAndHelpers.kt
+++ LoadingDialogs/src/main/java/bayern/kickner/loadingdialogs/ExtensionsAndHelpers.kt
@@ -4,5 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 
+/**
+ * This is a shortcut for remember { mutableStateOf(t) }.
+ * For Int, Double, Float on Android don't use this. There are optimized versions like mutableIntStateOf, etc.
+ */
 @Composable
 fun <T> rememberIt(t: T) = remember { mutableStateOf(t) }
